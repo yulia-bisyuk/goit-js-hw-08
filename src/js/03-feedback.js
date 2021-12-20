8884c08d561ac86969094f10f00de541f88dd2e2
yulia-bisyuk <yulia.bisyuk@gmail.com>
2021-12-21 00:37:09 +0200

    Task 3 started

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+// import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+refs = {
+    form: document.querySelector(".feedback-form"),
+    textArea: document.querySelector(".feedback-form textarea"),
+    submitBtn: document.querySelector(".feedback-form button"),
+}
+
+refs.form.addEventListener('input', onFormInput);
+const formData = {};
+
+function onFormInput(evt) {
+    
+    // formData[evt.target.name] = evt.target.value;
+    // console.log(formData);
+    console.log(evt.target.name);
+    console.log(evt.target.value);
+}
