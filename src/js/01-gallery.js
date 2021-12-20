b7e8631e563bad0b852b27dc5fa6748fc78b85df
yulia-bisyuk <yulia.bisyuk@gmail.com>
2021-12-20 20:54:48 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+    
+        return `<a class="gallery__item" href=${original}>
+  <img
+  class="gallery__image"
+  src=${preview} 
+  alt=${description}
+  title="${description}"
+  />
+                </a>`
+    }).join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
